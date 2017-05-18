21c9d025fc722d1a976afaf4e82153b0d3a20753
Ryan Cameron <camerorb@grace.edu>
2017-05-18 11:15:27 -0400

    add a promote button

--- js/app.js
+++ js/app.js
@@ -1,14 +1,87 @@
+$(document).foundation()
+
 const megaroster = {
+  students: [],
+
   init() {
+    this.studentList = document.querySelector('#student-list')
+    this.max = 0
+    this.setupEventListeners()
+  },
+
+  setupEventListeners() {
     document
       .querySelector('#new-student')
-      .addEventListener('submit', this.addStudent)
+      .addEventListener('submit', this.addStudent.bind(this))
   },
 
   addStudent(ev) {
     ev.preventDefault()
-    const studentName = ev.target.studentName.value
-    console.log(studentName)
+    const f = ev.target
+    const student = {
+      id: this.max + 1,
+      name: f.studentName.value,
+    }
+
+    this.students.unshift(student)
+    
+    const listItem = this.buildListItem(student)
+    this.prependChild(this.studentList, listItem)
+    
+    this.max ++
+    f.reset() 
+  
+     
   },
+
+  prependChild(parent, child) {
+    parent.insertBefore(child, parent.firstChild)
+  },
+
+  buildListItem(student) {
+    const template = document.querySelector('.student.template')
+    const li = template.cloneNode(true)
+    li.querySelector('.student-name').textContent = student.name
+    li.dataset.id = student.id
+    this.removeClassName(li, 'template')
+
+    li
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeStudent.bind(this))
+      
+    li
+      .querySelector('button.promote')
+      .addEventListener('click', this.changeColor)
+      
+    return li
+  },
+  removeStudent(ev) {
+    const btn = ev.target
+    const name = btn.closest('.student')
+    name.remove()
+    const id = name.getAttribute('data-id')
+    const index = this.findName(id)
+    this.students.splice(index, 1)
+
+  },
+  findName(index){
+    for(let i = 0; i< this.students.length; i++){
+      if (this.students[i].id==index){
+        return i
+      }
+    }
+  },
+  changeColor(ev){
+    const btn = ev.target
+    const name = btn.closest('.student')
+    name.classList.add("callout")
+    name.classList.add("primary")
+    name.classList.add("promoted")
+    console.log(name)
+  },
+  
+  removeClassName(el, className) {
+    el.className = el.className.replace(className, '').trim()
+  }
 }
 megaroster.init()
