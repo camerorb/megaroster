8988709c7ac39c30ccedda59662acee05da3a965
Ryan Cameron <camerorb@grace.edu>
2017-05-19 10:54:23 -0400

    In class work

--- js/app.js
+++ js/app.js
@@ -1,87 +1,142 @@
 $(document).foundation()
 
-const megaroster = {
-  students: [],
-
-  init() {
+class Megaroster {
+  constructor() {
     this.studentList = document.querySelector('#student-list')
+    this.students = []
     this.max = 0
     this.setupEventListeners()
-  },
+    this.load()
+  }
 
   setupEventListeners() {
     document
       .querySelector('#new-student')
-      .addEventListener('submit', this.addStudent.bind(this))
-  },
+      .addEventListener('submit', this.addStudentViaForm.bind(this))
+  }
 
-  addStudent(ev) {
+  load() {
+    const rosterString = localStorage.getItem('roster')
+    const rosterArray = JSON.parse(rosterString)
+    if (rosterArray) {
+      rosterArray
+        .reverse()
+        .map(this.addStudent.bind(this))
+    }
+  }
+
+  addStudentViaForm(ev) {
     ev.preventDefault()
     const f = ev.target
     const student = {
       id: this.max + 1,
       name: f.studentName.value,
     }
+    this.addStudent(student)
+    f.reset()
+  }
 
+  addStudent(student) {
     this.students.unshift(student)
     
     const listItem = this.buildListItem(student)
     this.prependChild(this.studentList, listItem)
     
-    this.max ++
-    f.reset() 
-  
-     
-  },
+    if (student.id > this.max) {
+      this.max = student.id
+    }
+
+    this.save()
+  }
 
   prependChild(parent, child) {
     parent.insertBefore(child, parent.firstChild)
-  },
+  }
 
   buildListItem(student) {
     const template = document.querySelector('.student.template')
     const li = template.cloneNode(true)
     li.querySelector('.student-name').textContent = student.name
+    li.setAttribute('title', student.name)
     li.dataset.id = student.id
-    this.removeClassName(li, 'template')
 
+    if (student.promoted) {
+      li.classList.add('promoted')
+    }
+
+    this.removeClassName(li, 'template')
+    this.setupActions(li, student)
+
+    return li
+  }
+
+  setupActions(li, student) {
     li
       .querySelector('button.remove')
       .addEventListener('click', this.removeStudent.bind(this))
-      
     li
       .querySelector('button.promote')
-      .addEventListener('click', this.changeColor)
+      .addEventListener('click', this.promoteStudent.bind(this, student))
+    li
+      .querySelector('button.move-up')
+      .addEventListener('click', this.moveUp.bind(this, student))
+  }
+
+  save() {
+    localStorage.setItem('roster', JSON.stringify(this.students))
+  }
+
+  moveUp(student, ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+
+    const index = this.students.findIndex((currentStudent, i) => {
+      return currentStudent.id === student.id
+    })
+
+    if (index > 0) {
+      this.studentList.insertBefore(li, li.previousElementSibling)
       
-    return li
-  },
+      const previousStudent = this.students[index - 1]
+      this.students[index - 1] = student
+      this.students[index] = previousStudent
+      this.save()
+    }
+  }
+
+  promoteStudent(student, ev) {
+    const btn = ev.target
+    const li = btn.closest('.student')
+    student.promoted = !student.promoted
+
+    if (student.promoted) {
+      li.classList.add('promoted')
+    } else {
+      li.classList.remove('promoted')
+    }
+
+    this.save()
+  }
+
   removeStudent(ev) {
     const btn = ev.target
-    const name = btn.closest('.student')
-    name.remove()
-    const id = name.getAttribute('data-id')
-    const index = this.findName(id)
-    this.students.splice(index, 1)
+    const li = btn.closest('.student')
 
-  },
-  findName(index){
-    for(let i = 0; i< this.students.length; i++){
-      if (this.students[i].id==index){
-        return i
+    for (let i=0; i < this.students.length; i++) {
+      let currentId = this.students[i].id.toString()
+      if (currentId === li.dataset.id) {
+        this.students.splice(i, 1)
+        break
       }
     }
-  },
-  changeColor(ev){
-    const btn = ev.target
-    const name = btn.closest('.student')
-    name.classList.add("callout")
-    name.classList.add("primary")
-    name.classList.add("promoted")
-    console.log(name)
-  },
-  
+
+    li.remove()
+    this.save()
+  }
+
   removeClassName(el, className) {
     el.className = el.className.replace(className, '').trim()
   }
 }
-megaroster.init()
+const roster = new Megaroster()
+//contentEditable
